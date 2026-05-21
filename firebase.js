f781151dde8ea7270c0ecb859c04c47432ada813
authorcartglobal <authorcartglobal@gmail.com>
2026-05-21 15:23:23 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB3-nmis6-ZtYSFAPd55zQerT99eABMqgY",
+  authDomain: "authorcartglobal-f9e0a.firebaseapp.com",
+  projectId: "authorcartglobal-f9e0a",
+  storageBucket: "authorcartglobal-f9e0a.firebasestorage.app",
+  messagingSenderId: "674505728964",
+  appId: "1:674505728964:web:80460e771b3a4ba39d7a91",
+  measurementId: "G-9HN2VR24QR"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app };
